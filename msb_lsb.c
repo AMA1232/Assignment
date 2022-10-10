0500c3ac5fb85aa3dc32ba7e5c096981f5bc8f7a
Adam Ajour <ama272@mail.aub.edu>
2022-10-10 23:31:04 +0300

    msb_lsb without specifications

---
+++ msb_lsb.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int msb(unsigned int num);
+
+int lsb(unsigned int num);
+
+const int NUMBER_OF_INTEGER_BITS = 32;
+
+int main(){
+    int num;
+    printf("Enter a poistive integer : ");
+    scanf("%d" , &num);
+    while(num <= 0){
+        printf("Invalid input. Enter a poistive integer : ");
+        scanf("%d" , &num);
+    }
+    int x = msb(num);
+    int y = lsb(num);
+
+    printf("The distance between the first and last bits is %d" , x-y);
+    return 0;
+}
+
+int msb(unsigned int num){
+    int k = 0;
+
+    while (num) {  // while num is not 0 keep shifting num to the right by 1 to find the msb
+        k++;
+        num *= 2; // shifting to the right y 1 is equivalent to multiplying by 2 power 1
+    }
+
+    return k - 1; // subtract 1 because its 0 indexed
+
+}
+
+int lsb(unsigned int num){
+    int k = NUMBER_OF_INTEGER_BITS;
+
+    while(num){
+        k--;
+        num /= 2;//shift to the left by 1 bit through dividing by 2 
+    }
+    return k;
+}
